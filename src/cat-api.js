bca5ebcd7a22989c5080d716e323d19ae4517846
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2023-12-15 06:26:24 +0100

    feat: update x-api-key

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
-  'live_FGH9lasd0oul4RhK4aaXXVevkdqzPmluPpIdqRAPTVq9GAtoohZ7VPmrFhJI1oqr';
+  'live_cPGyQpOwKH5NMhDgf9vtVO1fbcA1n7Y6XPZgHBDAg7Ml8U9Ljk0VzEWSFGH5jW2C';
 
 export const fetchBreeds = () => {
   return axios.get('https://api.thecatapi.com/v1/breeds');
